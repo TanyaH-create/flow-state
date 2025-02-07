476debfc714b7d33de7b334785676b5e2cf31cb3
Tanya <dougtanyah@gmail.com>
2025-02-07 12:33:04 -0500

    refactor: removed connect.ts - sequelize connection in modals/index.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
 
 //server.ts 
 import express from 'express';
-import sequelize from './config/connection.js';
-//import { sequelize } from './models/index.js';
+import { sequelize } from './models/index.js';
 import routes from './routes/index.js';
-
-console.log('Server.ts is loaded')
 
 
 const app = express();
@@ -15,8 +12,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('../client/dist'));
 app.use(express.json()); //parse all JSON request boodies
 app.use(routes); //mount all API endpoints from routes
-
-
 
 
 //.sync connects postgres database to server before starting the server
